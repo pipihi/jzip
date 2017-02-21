94ae3137bab68c0913987c293e7350bcbb1bf5cb
pipihi <2422636021@qq.com>
2017-02-21 23:04:05 +0800

    1、test.

--- src/com/pipihi/jzip/Zip.java
+++ src/com/pipihi/jzip/Zip.java
@@ -1,15 +1,38 @@
 package com.pipihi.jzip;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 public class Zip {
+	
+	private Zip mZip;
+	
 	public static final int UNZIP_ZIP_FILE_PATH_ERROR = 0;
+	
+	private Zip(){}
+	
+	public Zip getInstatnce() {
+		if(mZip == null) {
+			mZip = new Zip();
+		}
+		return mZip;
+	}
+	
 	public int unzip(String zipFilePath, String toDir) {
-		ZipFile file;
+		ZipFile file = null;
 		try {
 			file = new ZipFile(zipFilePath);
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath));
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		return 0;
